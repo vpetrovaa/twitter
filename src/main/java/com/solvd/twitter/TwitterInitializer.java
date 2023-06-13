d599d674160e71724e7ebae086fb9080f61d70e0
vpetrovaa <vpetrova.laba@solvd.com>
2023-06-13 15:23:34 +0300

    Project Structure was generated

---
+++ src/main/java/com/solvd/twitter/TwitterInitializer.java
@@ -0,0 +1,13 @@
+package com.solvd.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterInitializer.class, args);
+	}
+
+}
