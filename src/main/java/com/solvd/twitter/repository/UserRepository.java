d3a85fcc1bf7db5e5a767137c455b464358c101f
vpetrovaa <vpetrova.laba@solvd.com>
2023-06-14 16:13:12 +0300

    The order of methods in repository was changed

--- src/main/java/com/solvd/twitter/repository/UserRepository.java
+++ src/main/java/com/solvd/twitter/repository/UserRepository.java
@@ -11,12 +11,6 @@ public interface UserRepository extends Neo4jRepository<User, String> {
 
     boolean existsByEmail(String email);
 
-    @Query("MATCH (:User {id: $id})<-[:FOLLOW]-(followers:User) RETURN followers")
-    List<User> findFollowersByUserId(String id);
-
-    @Query("MATCH (:User {id: $id})-[:FOLLOW]->(followings:User) RETURN followings")
-    List<User> findFollowingsByUserId(String id);
-
     @Query(value = "MATCH (u:User), (f:User) WHERE u.id=$id  AND f.id=$followerId " +
             "CREATE (u)<-[r:FOLLOW]-(f) return f")
     User createFollowerById(@Param("id") String id, @Param("followerId") String followerId);
@@ -24,6 +18,12 @@ public interface UserRepository extends Neo4jRepository<User, String> {
     @Query(value = "MATCH (u:User), (f:User) WHERE u.id=$id  AND f.id=$followingId " +
             "CREATE (u)-[r:FOLLOW]->(f) return f")
     User createFollowingById(@Param("id") String id, @Param("followingId") String followingId);
+
+    @Query("MATCH (:User {id: $id})<-[:FOLLOW]-(followers:User) RETURN followers")
+    List<User> findFollowersByUserId(String id);
+
+    @Query("MATCH (:User {id: $id})-[:FOLLOW]->(followings:User) RETURN followings")
+    List<User> findFollowingsByUserId(String id);
 
     @Query("MATCH (u:User {id: :#{#user.id}}) SET u.name = :#{#user.name}, " +
             "u.surname = :#{#user.surname}, u.dateOfBirth = :#{#user.dateOfBirth} RETURN u")
