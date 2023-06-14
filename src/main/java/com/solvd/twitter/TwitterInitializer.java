51e70c6e091b211adb9e4c69c82dbf835ca8f3fe
vpetrovaa <vpetrova.laba@solvd.com>
2023-06-14 15:24:24 +0300

    Some properties for configuration were added

--- src/main/java/com/solvd/twitter/TwitterInitializer.java
+++ src/main/java/com/solvd/twitter/TwitterInitializer.java
@@ -2,11 +2,12 @@ package com.solvd.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TwitterInitializer {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(TwitterInitializer.class, args);
 	}
 
